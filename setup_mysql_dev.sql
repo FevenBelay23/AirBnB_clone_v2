659bc8c747020552ec418e4792a9d52864b585ea
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-11-14 00:52:54 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,7 @@
--- Creates a MySQL server with:
---   Database ezekielmisgae.
---   User ezekielmisgae with password ezekielmisgae in localhost.
---   Grants all privileges for ubuntu on 100.27.11.71.
---   Grants SELECT privilege for ubuntu on performance_schema.
+-- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS ezekielmisgae;
-CREATE USER
-    IF NOT EXISTS 'ubuntu'@'100.27.11.71'
-    IDENTIFIED BY 'ezekielmisgae';
-GRANT ALL PRIVILEGES
-   ON `ezekielmisgae`.*
-   TO 'ubuntu'@'100.27.11.71'
-   IDENTIFIED BY 'ezekielmisgae';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'ubuntu'@'100.27.11.71'
-   IDENTIFIED BY 'ezekielmisgae';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
