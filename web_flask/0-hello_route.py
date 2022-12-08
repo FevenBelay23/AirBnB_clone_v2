2f6a51d34f2bc4d297b615c109cf25b8064a9944
Ubuntu <ezkielmisgae@gmail.com>
2022-12-08 22:00:03 +0000

    COmmit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Simple Flask web application"""
+
 from flask import Flask
-app = Flask('__name__p')
+
+app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_route():
     """Return simple string"""
     return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
